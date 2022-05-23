86437fe5fdcf81f742884660f799d686873441f8
DucAnh <ducanh2k2@gmail.com>
2022-05-23 18:33:31 +0700

    source code

---
+++ luyencode/LATGACH.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#define LL long long 
+#define ULL unsigned long long
+#define FOR(i,a,b) for(int i=a;i<=b;++i)
+#define FO(i,a,b) for(int i=a;i<b;++i)
+#define FORD(i,a,b) for(int i=a;i>=b;--i)
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define debug cout << "YES" << endl
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+typedef pair<int,int> pII;
+typedef pair<LL,LL> pLL;
+const int IMAX = 1e9;
+const LL LMAX = (LL) 1e18;
+const LL MOD = 1000000007LL;
+#define PI 3.141592653589793238;
+#define maxn 100005
+
+void solve() {
+    int n;
+    cin >> n;
+    LL f[n+5] = {};
+    f[1] = 1;
+    f[2] = 2;
+    FOR(i,3,n) {
+        f[i] = f[i-2] + f[i-1];
+    }
+    cout << f[n] << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("test.in","r",stdin);
+    freopen("test.out","w",stdout);
+#endif
+	int T = 1;
+	cin >> T;
+	while( T-- )
+    	solve();
+    return 0;
+}
