5ab3f3cf1691b1398a1df3079d281bab87fff6a4
DucAnh <ducanh2k2@gmail.com>
2022-05-30 01:56:54 +0700

    source code

---
+++ vnoi/atcoder_dp_g.cpp
@@ -0,0 +1,66 @@
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#define LL long long 
+#define ULL unsigned long long
+#define FOR(i,a,b) for(int i=a;i<=b;++i)
+#define FO(i,a,b) for(int i=a;i<b;++i)
+#define FORD(i,a,b) for(int i=a;i>=b;--i)
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define debug cout << "YES" << endl
+#define all(x) x.begin(),x.end()
+using namespace std;
+
+typedef pair<int,int> pII;
+typedef pair<LL,LL> pLL;
+const int IMAX = 1e9;
+const LL LMAX = (LL) 1e18;
+const LL MOD = 1000000007LL;
+#define PI 3.141592653589793238;
+#define maxn 100005
+
+vector<int> g[maxn] = {};
+int f[maxn] = {},ans = 0;
+
+void dfs(int u) {
+    for(auto v:g[u]) {
+        if(!f[v]) {
+            dfs(v);
+        }
+        f[u] = max(f[u],f[v]);
+    }
+    f[u]++;
+    ans = max(ans,f[u]);
+}
+
+void solve() {
+    int n,m;
+    cin >> n >> m;
+    while(m--) {
+        int u,v;
+        cin >> u >> v;
+        g[u].pb(v);
+    }
+    FOR(i,1,n) {
+        if(!f[i]) {
+            dfs(i);
+        }
+    }
+    cout << ans-1 << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("test.in","r",stdin);
+    freopen("test.out","w",stdout);
+#endif
+	int T = 1;
+//	cin >> T;
+	while( T-- )
+    	solve();
+    return 0;
+}
