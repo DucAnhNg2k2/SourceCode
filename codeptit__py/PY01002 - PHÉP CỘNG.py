3bdc69f6e6f0d59f0ba55ffcf1b562b790307362
DucAnh <ducanh2k2@gmail.com>
2022-08-25 02:53:26 +0700

    source code

---
+++ codeptit__py/PY01002 - PHÉP CỘNG.py
@@ -0,0 +1,15 @@
+import math;
+
+t = 1
+# t = int(input())
+
+def solve():
+    n = input()
+    if(int(n[0]) + int(n[4]) == int(n[8])):
+        print("YES")
+    else:
+        print("NO")
+    return
+
+for i in range(1,t+1,1):
+    solve()
---
+++ codeptit__py/PY01053 - SỐ CHIA HẾT CHO 3.py
@@ -0,0 +1,14 @@
+import math;
+
+t = 1
+t = int(input())
+
+def solve():
+    n = int(input())
+    if(n%3 == 0):
+        print("YES")
+    else:
+        print("NO")
+
+for i in range(0,t,1):
+    solve()
